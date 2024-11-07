2ff4bb85494005c2788ecd874dbfd17c8b189288
arvinder004 <asdhoul004@gmail.com>
2024-11-07 16:18:10 +0530

    completed frontend

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import NoteCard from "../../components/Cards/NoteCard";
+import { MdAdd } from "react-icons/md";
+import AddEditNotes from "./AddEditNotes";
+import Modal from "react-modal"
 
 const Home = () => {
+
+    const [openAddEditModal, setOpenAddEditModal] = useState({
+        isShown: false,
+        type: "add",
+        data: null,
+    });
+
     return (
         <>
             <Navbar />
@@ -10,16 +20,65 @@ const Home = () => {
             <div className="container mx-auto">
                 <div className="grid grid-cols-3 gap-4 mt-8 ">
                     <NoteCard
-                        title="meeting on 7 april"
-                        date={"3 april"}
+                        title="AWS Cloud Practitioner"
+                        date={"3 april 2022"}
                         content="asdasdifbvsjd sdivu asd sdi eipsufhiweufn iweufh kdjfvn diufhiu i sdi dsiwf iscn sdfnsdvbn srgh"
-                        tags="#meeting"
+                        tags={"#AWS"}
+                        onEdit={() => { }}
+                        onDelete={() => { }}
+                        onPinNote={() => { }}
+                    />
+                    <NoteCard
+                        title="AWS Cloud Practitioner"
+                        date={"3 april 2022"}
+                        content="asdasdifbvsjd sdivu asd sdi eipsufhiweufn iweufh kdjfvn diufhiu i sdi dsiwf iscn sdfnsdvbn srgh"
+                        tags={"#AWS"}
+                        onEdit={() => { }}
+                        onDelete={() => { }}
+                        onPinNote={() => { }}
+                    /><NoteCard
+                        title="AWS Cloud Practitioner"
+                        date={"3 april 2022"}
+                        content="asdasdifbvsjd sdivu asd sdi eipsufhiweufn iweufh kdjfvn diufhiu i sdi dsiwf iscn sdfnsdvbn srgh"
+                        tags={"#AWS"}
+                        onEdit={() => { }}
+                        onDelete={() => { }}
+                        onPinNote={() => { }}
+                    /><NoteCard
+                        title="AWS Cloud Practitioner"
+                        date={"3 april 2022"}
+                        content="asdasdifbvsjd sdivu asd sdi eipsufhiweufn iweufh kdjfvn diufhiu i sdi dsiwf iscn sdfnsdvbn srgh"
+                        tags={"#AWS"}
                         onEdit={() => { }}
                         onDelete={() => { }}
                         onPinNote={() => { }}
                     />
                 </div>
             </div>
+
+            <button
+                className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10"
+                onClick={() => { setOpenAddEditModal({ isShown: true, type: "add", data: null }); }}>
+                <MdAdd className="text-[32px] text-white" />
+            </button>
+
+            <Modal
+                isOpen={openAddEditModal.isShown}
+                onRequestClose={() => { }}
+                style={{
+                    overlay: {
+                        backgroundColor: "rgba(0,0,0,0.2)",
+                    },
+                }}
+                contentLabel=""
+                className="w-[48%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
+            >
+                <AddEditNotes type={openAddEditModal.type} noteData={openAddEditModal.data} onClose={() => {
+                    setOpenAddEditModal({ isShown: false, type: "add", data: null })
+                }} />
+            </Modal>
+
+
         </>
     )
 }
